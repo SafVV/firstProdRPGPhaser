d41ef6004ff88aa3d18a30341a885d78123efddc
Vladis <vsafon09@mail.ru>
2025-02-06 13:31:23 +0300

    06022025

--- src/scenes/home.ts
+++ src/scenes/home.ts
@@ -1,1 +1,21 @@
-export class Home {}
+import mapJson from '../assets/map1_1.json';
+import { LAYER, Tiles, TilesSize } from '../utils/constants';
+
+export class Home extends Phaser.Scene {
+    constructor() {
+        super('HomeScene');
+
+    }
+
+    preload() {
+        this.load.image(Tiles.Home, 'src/assets/durotar.png')
+        this.load.tilemapTiledJSON('map', 'src/assets/map1_1.json')
+    }
+
+    create() {
+        const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage(mapJson.tilesets[0].name, Tiles.Home, TilesSize.SIZE, TilesSize.SIZE);
+        const groundLauer = map.createLayer(LAYER.GROUND, tileset, 0, 0);
+        const wallsLauer = map.createLayer(LAYER.WALLS, tileset, 0, 0);
+    }
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const Tiles = {
+    'Home': 'Home'
+}
+
+export const TilesSize = {
+    SIZE : 32
+}
+
+export const LAYER = {
+    GROUND: 'ground',
+    WALLS: 'walls'
+}
